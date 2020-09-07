87af54c55f203146169fec3b3fda7ce0bbf2f5b6
bajusz1968 <66406341+bajusz1968@users.noreply.github.com>
2020-09-07 15:47:09 +0200

    init page

--- playground.js
+++ playground.js
@@ -1,1 +1,13 @@
 // Peldaprogramok
+/*let x = 14;
+console.log(x);
+if (x % 5 === 0) {
+    x += 10;
+} else if (x % 2 === 0) {
+    x *= 2;
+} else {
+    x -= 10;
+}
+console.log(x);*/
+
+
--- project.js
+++ project.js
@@ -1,1 +1,27 @@
 // App-ot epitjuk egyutt
+/*let tomeg = 97;
+console.log(tomeg);
+let magassag = 170;
+console.log(magassag);
+let nev = "apa";
+console.log(nev);
+
+let BMIertek = (tomeg / magassag / magassag) * 10000;
+
+console.log(nev + ":" + BMIertek);
+console.log(`Név: ${nev} BMI index: ${BMIertek}`);
+if (BMIertek < 18.5) {
+    console.log("Underweight");
+} else if (BMIertek < 25) {
+    console.log("Normal or Healthy Weight");
+} else if (BMIertek < 30) {
+    console.log("Overweight");
+} else if (BMIertek < 35) {
+    console.log("Obese (class I)");
+} else if (BMIertek < 40) {
+    console.log("Obese (class II)");
+} else if (BMIertek < 50) {
+    console.log("Obese (class III)");
+} else {
+    console.log("Obese (class IV)");
+}*/
